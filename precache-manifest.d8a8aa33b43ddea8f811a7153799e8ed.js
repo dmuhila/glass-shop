68a166cd2107ca901a7007f19b046d5b258c7417
Muhila D <925dmuhila@gmail.com>
2019-03-06 20:20:06 +0000

    Updates

---
+++ precache-manifest.d8a8aa33b43ddea8f811a7153799e8ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "349461707020e338594ecb5f09094187",
+    "url": "/glass-shop/static/media/empty_cart.34946170.jpg"
+  },
+  {
+    "revision": "62e96ce8a5b26ee3aab1e5e5ead5185d",
+    "url": "/glass-shop/static/media/cart.62e96ce8.jpg"
+  },
+  {
+    "revision": "f268fa0f32b01b4bbc971b44a7ce6b46",
+    "url": "/glass-shop/static/media/Glass.f268fa0f.jpg"
+  },
+  {
+    "revision": "d32aadde509f06baf53d",
+    "url": "/glass-shop/static/js/runtime~main.d32aadde.js"
+  },
+  {
+    "revision": "67321f67d48fb3ab8b5e",
+    "url": "/glass-shop/static/js/main.67321f67.chunk.js"
+  },
+  {
+    "revision": "ed18ffe1a350284c1f60",
+    "url": "/glass-shop/static/js/1.ed18ffe1.chunk.js"
+  },
+  {
+    "revision": "67321f67d48fb3ab8b5e",
+    "url": "/glass-shop/static/css/main.66a5a0de.chunk.css"
+  },
+  {
+    "revision": "976c68bd6bfdc8bcf693f8bafc569dbc",
+    "url": "/glass-shop/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/glass-shop/precache-manifest.d8a8aa33b43ddea8f811a7153799e8ed.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/glass-shop/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
